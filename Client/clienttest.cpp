56ce8f74e6c29a24fa2b8f7a7a8e18e449f9715e
Holden103 <s225227592@deakin.edu.au>
2025-05-19 15:16:17 +1000

    First test, server started anc client attempting to send messages but wireshark stating port unreachable

--- Client/clienttest.cpp
+++ Client/clienttest.cpp
@@ -3,5 +3,17 @@
 int main()
 {
     write_line("Client file test");
+    const string hostIP = "127.0.0.1";
+    const connection_type protocol = UDP;
+    const unsigned short portNum = 5432;
+    const string serverConnectionName = "server connection";
+
+    connection serverConnection = open_connection(serverConnectionName, hostIP, portNum, protocol);
+
+    // For UDP, no need to check is_connection_open
+    send_message_to("Hello this is client", serverConnection);
+
+    close_connection(serverConnection);
+
     return 0;
 }
--- Server/servertest.cpp
+++ Server/servertest.cpp
@@ -1,7 +1,27 @@
 #include "splashkit.h"
 
+using std::to_string;
+
 int main()
 {
     write_line("Server file test");
-    return 0;
+
+    server_socket serverTest;
+    const connection_type protocol = UDP;
+    const unsigned short portNum = 5432;
+    const string serverName = "testing server";
+
+
+    serverTest = create_server(serverName, portNum, protocol);
+
+    while(!has_messages(serverName))
+    {
+        write_line("Waiting for messages...");
+        delay(1000);
+    }
+
+    string receivedMessage = read_message_data(serverName);
+    write_line("Received message: " + receivedMessage);
+
+    close_server(serverName);
 }
